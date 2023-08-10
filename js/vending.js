fef8f3eeee520bab18cda882b0934b9a0da3b8d6
JeongMinE2 <alsdl7354@naver.com>
2023-08-10 17:21:32 +0900

    update_1

--- js/vending.js
+++ js/vending.js
@@ -14,6 +14,10 @@ const drink = [
   {drinkName : "트래비", price : 1000, stock : 5}
 ]
 
+// for (const drinks of drink) {
+//   document.getElementById("output_box").innerHTML = JSON.stringify(drinkName);
+// }
+
 // 자판기 초기 금액 페이지 출력
 function input() {
   document.getElementById("machine_total_balance").value = 1000;  // 자판기 총 잔액
@@ -24,34 +28,42 @@ input();  // 함수 실행
 
 // 100원 클릭 시
 document.getElementById("100").onclick = function() {
-  document.getElementById("output_box").innerHTML = `<h1>100원이 투입되었습니다.  지갑에 남은 금액은 ${My_wallet-100}원 입니다.</h1>`;
-  My_wallet -= 100;
-  input_amount += 100;
+  let output = document.getElementById("output_box");
+
+  if (100 <= My_wallet) {
+    output.insertAdjacentHTML("afterbegin", `<h2>100원이 투입되었습니다. 지갑에 남은 금액은 ${My_wallet-100}원 입니다.</h2>`);
+    My_wallet -= 100;
+    input_amount += 100;
+  } else {
+    output.insertAdjacentHTML("afterbegin", `<h2>지갑에 돈이 모자라서 100원을 투입할 수 없습니다.</h2>`);
+  }
   document.querySelector("#My_wallet").value = My_wallet;
 }
 
 // 500원 클릭 시
 document.getElementById("500").onclick = function() {
-  document.getElementById("output_box").innerHTML = `<h1>500원이 투입되었습니다. 지갑에 남은 금액은 ${My_wallet-500}원 입니다.</h1>`;
-  My_wallet -= 500;
-  input_amount += 500;
-  document.querySelector("#My_wallet").value = My_wallet;
+  let output = document.getElementById("output_box");
 
+  if (500 <= My_wallet) {
+    output.insertAdjacentHTML("afterbegin", `<h2>500원이 투입되었습니다. 지갑에 남은 금액은 ${My_wallet-500}원 입니다.</h2>`);
+    My_wallet -= 500;
+    input_amount += 500;
+  } else {
+    output.insertAdjacentHTML("afterbegin", `<h2>지갑에 돈이 모자라서 500원을 투입할 수 없습니다.</h2>`);
+  }
+  document.querySelector("#My_wallet").value = My_wallet;
 }
 
 // 1000원 클릭 시
 document.getElementById("1000").onclick = function() {
-  document.getElementById("output_box").innerHTML = `<h1>1000원이 투입되었습니다. 지갑에 남은 금액은 ${My_wallet-1000}원 입니다.</h1>`;
-  My_wallet -= 1000;
-  input_amount += 1000;
+  let output = document.getElementById("output_box");
+
+  if (1000 <= My_wallet) {
+    output.insertAdjacentHTML("afterbegin", `<h2>1000원이 투입되었습니다. 지갑에 남은 금액은 ${My_wallet-1000}원 입니다.</h2>`);
+    My_wallet -= 1000;
+    input_amount += 1000;
+  } else {
+    output.insertAdjacentHTML("afterbegin", `<h2>지갑에 돈이 모자라서 1000원을 투입할 수 없습니다.</h2>`);
+  }
   document.querySelector("#My_wallet").value = My_wallet;
 }
-
-// let click = document.querySelector(".click");
-
-// click.addEventListener("click", function() {
-//   document.getElementById("output_box").innerHTML = `<h1>1000원이 투입되었습니다. 지갑에 남은 금액은 ${My_wallet-1000}원 입니다.</h1>`;
-//   My_wallet -= 1000;
-//   input_amount += 1000;
-//   document.querySelector("#My_wallet").value = My_wallet;
-// })
